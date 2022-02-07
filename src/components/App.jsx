f1c94c2b9ddba3440172bd300c6f912b275746ac
AnnaSobchenko <annsbchnk@gmail.com>
2022-02-07 22:10:17 +0200

    components create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import './styles.css'
+
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+    <div class="photo-card">
+    <img src="{webformatURL}" alt="{tags}" loading="lazy"/>    
+    </div>);
+  }
+}
+ 
+export default App;
